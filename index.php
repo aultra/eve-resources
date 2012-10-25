8d9b86173c01734676b5b13395763017e3887d66
Jason Andrew <jason@andrewzoo.net>
2012-10-25 16:56:21 -0500

    Lots of changes to CSS, layout, and functionality.

--- index.php
+++ index.php
@@ -2,13 +2,18 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Phoenix Legion Resources Calculator</title>
+        <title>Phoenix Legion Resources Value Comparison</title>
         <?php
             require_once('includes/class.Resources.inc');
             require_once('includes/class.EveOreDB.inc');
         ?>
     </head>
     <body>
+        <h1>Phoenix Legion Resources Value Comparison</h1>
+        
+        <p>
+            Welcome to the Phoenix Legion resource value comparison tool.  This tool is intended for our players to be able to compare the value of each ore from 
+        </p>
         <?php
             $resources = new Resources();
             $resources->loadResources();
